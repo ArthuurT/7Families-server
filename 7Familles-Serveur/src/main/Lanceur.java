8af5aa2819ee722e7d2188fc8b1f56631cf4d787
Lastshot97 <juju7397@gmail.com>
2019-10-16 14:16:08 +0200

    Configuration file

--- 7Familles-Serveur/src/main/Lanceur.java
+++ 7Familles-Serveur/src/main/Lanceur.java
@@ -1,9 +1,13 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Properties;
 
 import model.Serveur;
 import remote.IServeur;
@@ -11,19 +15,35 @@ import remote.IServeur;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		int port = Integer.parseInt(args[0]);
-		System.setProperty(
-			"java.security.policy",
-			"file:///home/asao/Documents/Universite/Semestre9/Middlewares/7Families/7Families-server/7Familles-Serveur/src/security.policy");
-		if (System.getSecurityManager() == null) {
-		    System.setSecurityManager(new SecurityManager());
-		}
 		try {
+			String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+			String configPath = rootPath + "config.properties";
+			
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(configPath));
+			
+			String securityPolicyPath = "file:" + properties.getProperty("security.file.path");
+			System.setProperty("java.security.policy", securityPolicyPath);
+			if (System.getSecurityManager() == null) {
+			    System.setSecurityManager(new SecurityManager());
+			}
+			
+			int port = Integer.parseInt(properties.getProperty("server.port"));
+			String serverInterface = properties.getProperty("server.interface");
+			
 			IServeur serveur = new Serveur();
 			IServeur skeleton = (IServeur) UnicastRemoteObject.exportObject(serveur, 0);
 			Registry registry = LocateRegistry.createRegistry(port);
-			registry.rebind("serveur", skeleton);
+			registry.rebind(serverInterface, skeleton);
+			
+			System.out.println(String.format("Server running on port %d", port));
 		} catch(RemoteException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
